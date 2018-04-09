d21d5977cb7a3d3005d88923837c754c78b464d3
Aleksey Kravetz <aleksey.kravetz@gmail.com>
2018-04-09 17:58:18 +0300

    Install Font-Awesome Add PrimeNg, Font-Awesome CSS Add common modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
